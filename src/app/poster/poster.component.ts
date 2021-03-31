28979557e287d35ce7d7ee177c31d3de757b8708
FlareFlo <florianlipp17@gmail.com>
2021-03-31 20:12:14 +0200

    Added userinput for username and email

--- src/app/poster/poster.component.ts
+++ src/app/poster/poster.component.ts
@@ -10,9 +10,10 @@ export class PosterComponent implements OnInit {
 
   constructor(private  http: HttpClient) {
   }
+
   object = {
-    "username": 'foo',
-    "email": 'loo@gmail.com'
+    username: 'foo',
+    email: 'loo@gmail.com'
   };
 
   doPOST() {
@@ -21,7 +22,20 @@ export class PosterComponent implements OnInit {
     this.http.post(url, this.object).subscribe();
   }
 
+  doUsername(userinput: string) {
+    console.log('Writing: ' + userinput + ' over ' + this.object.username);
+    this.object.username = userinput;
+    console.log(this.object);
+  }
+
+  doEmail(emailinput: string) {
+    console.log('Writing: ' + emailinput + ' over ' + this.object.email);
+    this.object.email = emailinput;
+    console.log(this.object);
+  }
+
   ngOnInit(): void {
+    console.log(this.object);
   }
 
 }
