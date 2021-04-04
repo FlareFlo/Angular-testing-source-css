5eab3d696cece21c0335018734dd6a9f26ebd4c3
FlareFlo <florianlipp17@gmail.com>
2021-04-05 01:58:25 +0200

    Added specific input fields and changed input function

--- src/app/usermanagement/usermanagement.component.ts
+++ src/app/usermanagement/usermanagement.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-usermanagement',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsermanagementComponent implements OnInit {
 
-  constructor() { }
+  constructor(private  http: HttpClient) {
+  }
+
+  object = {
+    userid: 0,
+    username: '',
+    emailAddress: '',
+    password: ''
+  };
+
+  doPOST() {
+    console.log('Posting data...');
+    const url = 'https://backend.yap.dragoncave.dev/user';
+    this.http.post(url, this.object).subscribe();
+  }
+
+  doUID(idinput: string) {
+    console.log('Writing: ' + idinput + ' over ' + this.object.userid);
+    (this.object.userid) = parseInt(idinput);
+    console.log(this.object);
+  }
+
+  doUsername(userinput: string) {
+    console.log('Writing: ' + userinput + ' over ' + this.object.username);
+    this.object.username = userinput;
+    console.log(this.object);
+  }
+
+  doEmail(emailinput: string) {
+    console.log('Writing: ' + emailinput + ' over ' + this.object.emailAddress);
+    this.object.emailAddress = emailinput;
+    console.log(this.object);
+  }
+
+  doPassword(passwordinput: string) {
+    console.log('Writing: ' + passwordinput + ' over ' + this.object.password);
+    this.object.password = passwordinput;
+    console.log(this.object);
+  }
 
   ngOnInit(): void {
   }
-
 }
