8fec37a6f8d3a9123199592f13220b63b04c81a6
FlareFlo <florianlipp17@gmail.com>
2021-04-09 16:35:22 +0200

    Added client import

--- src/app/tokentester/tokentester.component.ts
+++ src/app/tokentester/tokentester.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-tokentester',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TokentesterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private  http: HttpClient) { }
+
 
   ngOnInit(): void {
   }
