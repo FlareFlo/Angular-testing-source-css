e32c4f50c85ee5dc51711878d6388f7d05c48781
FlareFlo <florianlipp17@gmail.com>
2021-04-14 17:48:43 +0200

    Implemented Cookie for GET/PUT system

--- src/app/usermanagement/usermanagement.component.ts
+++ src/app/usermanagement/usermanagement.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {CookieService} from 'ngx-cookie';
 
 @Component({
   selector: 'app-usermanagement',
@@ -8,7 +9,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 })
 export class UsermanagementComponent implements OnInit {
 
-  constructor(private  http: HttpClient) {
+  constructor(private  http: HttpClient, private cookieService: CookieService) {
   }
 
   readonly ROOT_URL = 'https://backend.yap.dragoncave.dev/user/';
@@ -16,34 +17,32 @@ export class UsermanagementComponent implements OnInit {
   getresponse = {};
 
   packageobject = {
-    userid: 0,
-    username: '',
-    emailAddress: '',
-    password: ''
+    username: 'testuser1',
+    emailAddress: 'test1@mail.com',
+    password: 'testpassword1'
   };
 
   idinput!: string;
 
+  token!: string;
+
   doPUT() {
-    const headerS = new HttpHeaders().set('Content-Type', 'application/json');
-    console.log('Putting data...');
-    console.log(this.packageobject);
-    this.http.put<any>(this.ROOT_URL + this.idinput, this.packageobject, {headers: headerS})
+    this.token = this.cookieService.get('token');
+    let headerS = new HttpHeaders().set('Content-Type', 'application/json');
+    headerS = headerS.append('Token', this.token);
+    this.http.put<any>(this.ROOT_URL, this.packageobject, {headers: headerS})
       .subscribe();
   }
 
-  getStuff(idinput: any) {
-    idinput = parseInt(idinput, 10);
-    this.getresponse = this.http.get<object>(this.ROOT_URL + idinput)
+  getStuff() {
+    this.token = this.cookieService.get('token');
+    let headerS = new HttpHeaders().set('Content-Type', 'application/json');
+    headerS = headerS.append('Token', this.token);
+    this.getresponse = this.http.get<object>(this.ROOT_URL, {headers: headerS})
       .toPromise()
       .then();
   }
 
-  doUID(idinput: string) {
-    this.idinput = idinput;
-    (this.packageobject.userid) = parseInt(idinput, 10);
-    this.getStuff(idinput);
-  }
 
   doUsername(userinput: string) {
     this.packageobject.username = userinput;
