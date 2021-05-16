4afce48ea62134720321df04883b95251b8bf02d
FlareFlo <florianlipp17@gmail.com>
2021-05-16 15:44:34 +0200

    Fixed local object and added commenting

--- src/app/usermanagement/usermanagement.component.ts
+++ src/app/usermanagement/usermanagement.component.ts
@@ -10,21 +10,21 @@ import {CookieService} from 'ngx-cookie';
 export class UsermanagementComponent implements OnInit {
 
   constructor(private  http: HttpClient, private cookieService: CookieService) {
+    this.getStuff();
   }
 
   readonly ROOT_URL = 'https://backend.yap.dragoncave.dev/user/';
 
-  getresponse = {};
 
   packageobject = {
-    username: 'testuser1',
-    emailAddress: 'test1@mail.com',
-    password: 'testpassword1'
+    username: '',
+    emailAddress: '',
+    password: ''
   };
 
-  idinput!: string;
-
   token!: string;
+
+  response: any; // temporary object for translating response to local object
 
   doPUT() {
     this.token = this.cookieService.get('token');
@@ -35,12 +35,18 @@ export class UsermanagementComponent implements OnInit {
   }
 
   getStuff() {
-    this.token = this.cookieService.get('token');
-    let headerS = new HttpHeaders().set('Content-Type', 'application/json');
-    headerS = headerS.append('Token', this.token);
-    this.getresponse = this.http.get<object>(this.ROOT_URL, {headers: headerS})
+    this.token = this.cookieService.get('token'); // get cookie from browser
+    let headerS = new HttpHeaders().set('Content-Type', 'application/json'); // define content as JSON
+    headerS = headerS.append('Token', this.token); // add token to header
+    this.http.get<object>(this.ROOT_URL, {headers: headerS})
       .toPromise()
-      .then();
+      .then(
+        res => {
+          this.response = res;
+          this.packageobject = this.response;
+          console.log(this.packageobject);
+        }
+      );
   }
 
 
