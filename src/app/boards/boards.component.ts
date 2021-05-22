b71c035f89c88a859c41fb200d5addb35c6ba396
FlareFlo <florianlipp17@gmail.com>
2021-05-22 15:05:41 +0200

    Transfer of styling and functions from entries to boards

--- src/app/boards/boards.component.ts
+++ src/app/boards/boards.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {CookieService} from 'ngx-cookie';
+import {MatDialog} from '@angular/material/dialog';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
 	selector: 'app-boards',
@@ -7,10 +11,30 @@ import {Component, OnInit} from '@angular/core';
 })
 export class BoardsComponent implements OnInit {
 
-	constructor() {
+	constructor(private http: HttpClient, private cookieService: CookieService, public dialog: MatDialog) {
+	}
+
+	boards = [
+		{
+			boardID: 0,
+			name: '',
+			createDate: 0
+		}
+	];
+
+	dragging!: boolean;
+
+	drop(event: CdkDragDrop<{ title: string, description: string }[]>) {
+		moveItemInArray(this.boards, event.previousIndex, event.currentIndex);
+	}
+
+	handleClick($event: MouseEvent) {
+		if (this.dragging) {
+			this.dragging = false;
+			return;
+		}
 	}
 
 	ngOnInit(): void {
 	}
-
 }
